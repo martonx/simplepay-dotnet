2a8ad0c6bb92db8a08a6c4e1354059eea49e6ee3
florian.marton@mortons.hu <florian.marton@mortons.hu>
2020-02-13 10:55:58 +0100

    conflicts

--- SimplePayment/Common/Models/OrderDetails.cs
+++ SimplePayment/Common/Models/OrderDetails.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace SimplePayment.Common.Models
 {
-    public class OrderDetails
+    public class OrderDetails : BaseOrderDetails
     {
         public string Merchant { get; set; }
         public string OrderRef { get; set; }
@@ -12,7 +12,7 @@ namespace SimplePayment.Common.Models
         public string Salt { get; set; }
         public string Customer { get; set; }
         public string CustomerEmail { get; set; }
-        public bool TwoStep { get; set; }
+        public bool? TwoStep { get; set; }
         public string Language { get; set; }
         public string SDKVersion { get; set; }
         public string[] Methods { get; set; }
