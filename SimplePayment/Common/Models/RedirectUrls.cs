52346ecaf82c8a8ba96b32758ef0abc2d79e40f5
florian.marton@mortons.hu <florian.marton@mortons.hu>
2020-04-23 17:57:13 +0200

    redirect urls added

---
+++ SimplePayment/Common/Models/RedirectUrls.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimplePayment.Common.Models
+{
+    public class RedirectUrls
+    {
+        public string Success { get; set; }
+        public string Fail { get; set; }
+        public string Timeout { get; set; }
+        public string Cancel { get; set; }
+    }
+}
